5c7a8f2088295013534fc664ff6556c03a3e9c9a
medAli <khemirimohamedali7@gmail.com>
2022-08-29 21:56:31 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,36 @@ import Home from "./pages/Home";
 import NewsList from "./pages/NewsList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import Cart from "./pages/Cart";
+
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from "react-router-dom";
 
 const App = () => {
-	return <Home />;
+	const user = true;
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route path="/news">
+					<NewsList />
+				</Route>
+				<Route path="/new/:id">
+					<New />
+				</Route>
+
+				<Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+				<Route path="/register">
+					{user ? <Redirect to="/" /> : <Register />}
+				</Route>
+			</Switch>
+		</Router>
+	);
 };
 
 export default App;
--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,57 +1,59 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { mobile } from "../responsive";
 
 const Container = styled.div`
-  flex: 1;
-  margin: 3px;
-  height: 50vh;
-  position: relative;
+	flex: 1;
+	margin: 3px;
+	height: 50vh;
+	position: relative;
 `;
 
 const Image = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  ${mobile({ height: "20vh" })}
-
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+	${mobile({ height: "20vh" })}
 `;
 
 const Info = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
 `;
 
 const Title = styled.h1`
-    color:white;
-    margin-bottom: 20px;
+	color: white;
+	margin-bottom: 20px;
 `;
 
 const Button = styled.button`
-    border:none;
-    padding: 10px;
-    background-color: white;
-    color:gray;
-    cursor: pointer;
-    font-weight: 600;
+	border: none;
+	padding: 10px;
+	background-color: white;
+	color: gray;
+	cursor: pointer;
+	font-weight: 600;
 `;
 
 const CategoryItem = ({ item }) => {
-  return (
-    <Container>
-      <Image src={item.img} />
-      <Info>
-        <Title>{item.title}</Title>
-        <Button>Lire la suite</Button>
-      </Info>
-    </Container>
-  );
+	return (
+		<Container>
+			<Link to={`/news/${item.cat}`}>
+				<Image src={item.img} />
+				<Info>
+					<Title>{item.title}</Title>
+					<Button>Lire la suite</Button>
+				</Info>
+			</Link>
+		</Container>
+	);
 };
 
 export default CategoryItem;
